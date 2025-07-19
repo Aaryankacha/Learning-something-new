6cdedd8c3eb3adc5849a0bd0869122d344f4a08f
unknown <aryannkacha@gmail.com>
2025-07-19 18:57:30 +0530

    Dart basics

---
+++ Dart-basic/list_set_map.dart
@@ -0,0 +1,11 @@
+void main(){
+  List<String> movies= ["The Social network","La la land","Fight club"];
+  Set<int> number={2,4,7,3,4,3,12};
+  Map<String,int> friends={
+    "ravi": 23,
+    "akash": 21
+  };
+  print(movies);
+  print(number);
+  print(friends);
+}
